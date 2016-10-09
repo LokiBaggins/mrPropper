855df05b948bedecde5d8bd7420daaf8d4c8d473
Alex Baggins <loki_ord@rambler.ru>
2016-10-09 19:58:04 +0300

    Removed tutorial tails

--- src/main/java/by/baggins/App.java
+++ src/main/java/by/baggins/App.java
@@ -1,5 +1,8 @@
 package by.baggins;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import by.baggins.controller.DuplicatesController;
 import by.baggins.dto.FileInfo;
 import javafx.application.Application;
@@ -10,17 +13,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 public class App extends Application {
     private ObservableList<FileInfo> fileInfoList = FXCollections.observableArrayList();
-
-    public App() {
-//        fileInfoList.add(new FileInfo(2.4d, "file1", "txt"));
-//        fileInfoList.add(new FileInfo(2.5d, "file2", "xml"));
-//        fileInfoList.add(new FileInfo(2.6d, "file3", "xsl"));
-    }
 
     public static void main(String[] args) throws Exception {
         launch(args);
@@ -39,7 +33,6 @@ public class App extends Application {
             Parent root = (Parent) loader.load(inputStream);
 
             DuplicatesController dupController = loader.getController();
-            dupController.setApp(this);
 
             stage.setTitle("mrPropper");
             stage.setScene(new Scene(root));
